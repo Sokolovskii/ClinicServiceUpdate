e860fc2c07498829f08d8163d669f62e6771843d
Sokolovskii Alexander <sokolovskii.alex2@gmail.com>
2021-07-23 19:08:46 +1000

    Добавлены хранимые процедуры по работе с заявками

---
+++ SQL Scripts/Add_New_Request.sql
@@ -0,0 +1,17 @@
+﻿USE [ClinicService]
+GO
+
+--===================
+--Author: Sokolovskiy Alexander
+--Date: 23.07.21
+--Description: Формирование заявки
+--===================
+
+CREATE PROCEDURE AddNewRequest
+	@SenderId INT,
+	@RecieverId INT,
+	@TypeId INT,
+	@Content NVARCHAR(MAX)
+AS
+INSERT INTO dbo.Requests(Sender_Id, Reciever_Id, [Type_Id], Content, Date_Of_Create)
+	VALUES (@SenderId, @RecieverId, @TypeId, @Content, GETDATE())
---
+++ SQL Scripts/Get_Open_Requests_For_Sender.sql
@@ -0,0 +1,22 @@
+﻿USE [ClinicService]
+GO
+
+--===================
+--Author: Sokolovskiy Alexander
+--Date: 23.07.21
+--Description: Возвращает все открытые заявки пользователя  
+--===================
+
+CREATE PROCEDURE GetOpenRequestsForSender
+	@UserId INT
+AS
+SELECT
+	r.Id,
+	r.Date_Of_Create,
+	r.Content,
+	u.Name,
+	tor.Name
+FROM dbo.Requests r
+JOIN dbo.Users u ON u.Id = r.Reciever_Id
+JOIN dbo.TypesOfRequests tor ON tor.Id = r.Type_Id
+WHERE r.Sender_Id = @UserId AND Date_Of_Complete IS NULL
